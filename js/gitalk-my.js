31ca57693e928f0eb91b4a2b04ab7fd193ff45f5
ali3707 <66162729+ali3707@users.noreply.github.com>
2020-06-10 15:31:03 +0800

    2

--- js/gitalk-my.js
+++ js/gitalk-my.js
@@ -3,11 +3,11 @@
 function lablegen(hook) {
     hook.doneEach(function () {
         gitalk = new Gitalk({
-            clientID: 'ced6988e6ccedf962b67',
-            clientSecret: '73a32399b7eda1c3142aa163775550aa214b056a',
-            repo: 'zshorz.github.io',
-            owner: 'zshorz',
-            admin: ['zshorz'],
+            clientID: '4da4ca1fa8befbc8dcb6',
+            clientSecret: 'f5b55cb38dd34f6218a0974f89011d72fc4d1514',
+            repo: 'ali3707.github.io',
+            owner: 'ali3707',
+            admin: ['ali3707'],
             id: hex_md5(window.location.pathname + window.location.hash.split('?')[0]),
             distractionFreeMode: true
         });
